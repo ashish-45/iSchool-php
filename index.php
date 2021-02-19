9fee7d45bac9ac1f848a549e04a133748664dc5d
ashish-45 <ashishnirvikar5670@gmail.com>
2021-02-19 16:08:58 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+    <link href="Css/style1.css" rel="stylesheet">
+    <title>ELearning</title>
+</head>
+<body>
+
+<!-- Start Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark pl-5 fixed-top">
+  <a class="navbar-brand" href="index.php">iSchool</a>
+  <span class="navbar-text">Learn and Implement</span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav custom-nav pl-5">
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item custom-nav-item" >
+        <a class="nav-link" href="#">Courses</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Payment Status</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">My Profile</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Logout</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Signup</a>
+      </li>
+       <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Feedback</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<!-- End Navigation -->
+
+<!-- Start Video background -->
+
+<div class="container-fluid remove-vid-marg">
+  <div class="vid-parent">
+    <video playsinline autoplay muted Loop>
+      <Source src="videos/Notebook1.vid">
+    </video>
+    <div class="vid-overlay">
+    </div>
+  </div>
+  <div class="vid-content">
+    <h1 class="my-content">Welcome To iSchool</h1>
+    <small class="my-content" >Learn and Inplement</small><br>
+    <a href="#" class="btn btn-danger ">Get Started</a>
+  </div>
+</div>
+<!-- End Video background -->
+
+
+<!-- start text banner -->
+
+<div class="container-fuild bg-danger txt-banner" style="margin-top:-6px">
+  <div class="row bottom-banner">
+    <div class="col-sm">
+      <h5><i class="fas fa-book-open mr-3"></i> 100+ online Courses</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-users mr-3"></i> Expert Infrastructure</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-keyboard mr-3"></i> Lifetime Acess</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-rupee-sign mr-3"></i> Money back Guarantee</h5>
+    </div>
+  </div>
+</div>
+<!-- End text banner -->
+
+
+<!-- start popular course -->
+<div class="container mt-5">
+  <h1 class="text-center">Popular Course</h1>
+  <div class="card-deck mt-4">
+    <a href="#" class="btn" style="text-align:left; padding:0px; margin:0px;">
+    <div class="card">
+      <img src="images/Python.jpg" class="card-img-top" alt="python"/>
+      <div class="card-body">
+        <h5 class="card-title">Learn Python</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laborum numquam accusantium ex explicabo voluptatum id, maiores odit quod eos quia aliquid vero nulla aut debitis inventore aperiam similique. Omnis!</p>
+      </div>
+      <div class="card-footer">
+        <p class="card-text d-inline">Price : <small><del>& #8377 2000</del></small>
+      <span class="font-weight-bolder">&#8377 200</span></p>
+      <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+      </div>
+    </div>
+ </a>
+
+ <a href="#" class="btn" style="text-align:left; padding:0px; margin:0px;">
+    <div class="card">
+      <img src="images/Python.jpg" class="card-img-top" alt="python"/>
+      <div class="card-body">
+        <h5 class="card-title">Learn Python</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laborum numquam accusantium ex explicabo voluptatum id, maiores odit quod eos quia aliquid vero nulla aut debitis inventore aperiam similique. Omnis!</p>
+      </div>
+      <div class="card-footer">
+        <p class="card-text d-inline">Price : <small><del>& #8377 2000</del></small>
+      <span class="font-weight-bolder">&#8377 200</span></p>
+      <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+      </div>
+    </div>
+ </a>
+
+ <a href="#" class="btn" style="text-align:left; padding:0px; margin:0px;">
+    <div class="card">
+      <img src="images/Python.jpg" class="card-img-top" alt="python"/>
+      <div class="card-body">
+        <h5 class="card-title">Learn Python</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laborum numquam accusantium ex explicabo voluptatum id, maiores odit quod eos quia aliquid vero nulla aut debitis inventore aperiam similique. Omnis!</p>
+      </div>
+      <div class="card-footer">
+        <p class="card-text d-inline">Price : <small><del>& #8377 2000</del></small>
+      <span class="font-weight-bolder">&#8377 200</span></p>
+      <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+      </div>
+    </div>
+ </a>
+  </div>
+
+  <div class="card-deck mt-4">
+    <a href="#" class="btn" style="text-align:left; padding:0px; margin:0px;">
+    <div class="card">
+      <img src="images/JavaScript.jpg" class="card-img-top" alt="javascript"/>
+      <div class="card-body">
+        <h5 class="card-title">Learn JavaScript</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laborum numquam accusantium ex explicabo voluptatum id, maiores odit quod eos quia aliquid vero nulla aut debitis inventore aperiam similique. Omnis!</p>
+      </div>
+      <div class="card-footer">
+        <p class="card-text d-inline">Price : <small><del>& #8377 2000</del></small>
+      <span class="font-weight-bolder">&#8377 200</span></p>
+      <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+      </div>
+    </div>
+ </a>
+
+ <a href="#" class="btn" style="text-align:left; padding:0px; margin:0px;">
+    <div class="card">
+      <img src="images/JavaScript.jpg" class="card-img-top" alt="javascript"/>
+      <div class="card-body">
+        <h5 class="card-title">Learn JavaScript</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laborum numquam accusantium ex explicabo voluptatum id, maiores odit quod eos quia aliquid vero nulla aut debitis inventore aperiam similique. Omnis!</p>
+      </div>
+      <div class="card-footer">
+        <p class="card-text d-inline">Price : <small><del>& #8377 2000</del></small>
+      <span class="font-weight-bolder">&#8377 200</span></p>
+      <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+      </div>
+    </div>
+ </a>
+
+ <a href="#" class="btn" style="text-align:left; padding:0px; margin:0px;">
+    <div class="card">
+      <img src="images/JavaScript.jpg" class="card-img-top" alt="javascript"/>
+      <div class="card-body">
+        <h5 class="card-title">Learn JavaScript</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laborum numquam accusantium ex explicabo voluptatum id, maiores odit quod eos quia aliquid vero nulla aut debitis inventore aperiam similique. Omnis!</p>
+      </div>
+      <div class="card-footer">
+        <p class="card-text d-inline">Price : <small><del>& #8377 2000</del></small>
+      <span class="font-weight-bolder">&#8377 200</span></p>
+      <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+      </div>
+    </div>
+ </a>
+  </div>
+
+  
+  <div class="text-center m-2">
+    <a class="btn btn-danger btn-sm" href="#">View All Course</a>
+  </div>
+</div>
+<!-- end popular course -->
+
+<!-- Start Contact Us Page -->
+<div class="container mt-4">
+  <h2 class="tect-center mb-2">Contact Us</h2>
+  <div class="row">
+    <div class="col-md-8">
+      <form action="" method="post">
+        <input type="text" class="form-control" name="name" placeholder="Name"><br>
+        <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+        <input type="text" class='form-control' name="email" placeholder="Email"><br>
+        <textarea type="text" class='form-control' name="message" placeholder="How we can help you"></textarea><br>
+        <input type="submit" class="btn btn-primary" value="Send" name="submit"><br><br>
+      </form>
+    </div>
+    <div class="col-md-4 stripe text-white text-center" style="background-color:  #93291e;">
+      <h4>iSchool</h4>
+      <p>iSchool,
+        Near Police Camp II Bhiwandi,
+        Mumbai - 400123<br/>
+        Phone : +0000123 <br/>
+        www.iSchool.com
+      </p>
+    </div>
+  </div>
+</div>
+<!-- End Contact Us page -->
+
+
+<!-- start Student Testimonial -->
+
+
+<div class="container-fluid mt-5" style="background-color:#487289" id="Feedback">
+  <h1 class="text-center testheading p-4">Student's Feedback</h1>
+  <div class="row">
+    <div class="col-md-12">
+      <div id="testimonial-slider" class="owl-carosel">
+        <div class="testimonial">
+          <p class="description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum ut ratione aliquid maxime reprehenderit consequuntur iusto nesciunt tempore sit, fugit recusandae amet tempora, doloremque officia, a tenetur? Iure, provident explicabo.
+          </p>
+          <div class="pic">
+            <img src="images/jonh.png" alt=""  style="height:100px;"/>
+          </div>
+          <div class="testimonial-proof">
+            <h4>Ashish Nirvikar</h4>
+            <small>Full Stack Developer</small>
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Student Testimonial -->
+
+<!-- Social site-->
+<div class="container-fluid bg-danger">
+  <div class="row text-white text-center p-1">
+    <div class="col-sm">
+      <a class="text-white social-hover" href="#"><i class="fab fa-facebook"></i> Facebook</a>
+    </div>
+    <div class="col-sm">
+      <a class="text-white social-hover" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+    </div>
+    <div class="col-sm">
+      <a class="text-white social-hover" href="#"><i class="fab fa-whatsapp"></i> Whatsapp</a>
+    </div>
+    <div class="col-sm">
+      <a class="text-white social-hover" href="#"><i class="fab fa-instagram"></i> instagram</a>
+    </div>
+  </div>
+</div>
+<!-- end social site -->
+
+<!-- start about section -->
+<div class="container-fluid p-4" style="background-color:#E9ECEF">
+<div class="container" style="background-color:#E9CEF">
+<div class="row text-center">
+  <div class="col-sm">
+    <h5>About Us</h5>
+    <p>iSchool Provide Universal Acess to the World's best
+       Education partnering with top universities and
+      organization to offer courses online.</p>
+  </div>
+  <div class="col-sm">
+    <h5>Catogary</h5>
+    <a class="text-dark" href="#">Web Development</a><br/>
+    <a class="text-dark" href="#">Android Development</a><br/>
+    <a class="text-dark" href="#">Ios Development</a><br/>
+    <a class="text-dark" href="#">Data Analyst </a><br/>
+    <a class="text-dark" href="#">Machine Learning</a><br/>
+  </div>
+  <div class="col-sm">
+    <h5>Contact Us</h5>
+    <p>iSchool Pvt Ltd <br> Near Police Camp II <br> Bhiwandi,
+  Mumbai<br> ph.00000000</p>
+  </div>
+</div>
+</div>
+
+</div>
+</div>
+<!-- End About section -->
+
+<!-- start Footer -->
+<footer class="container-fluid bg-dark text-center p-2">
+<small class="text-white">Copyright &copy; 2021 || Designed By E-Learning || Admin Login </small>
+</footer>
+<!-- End footer -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+</body>
+</html>
